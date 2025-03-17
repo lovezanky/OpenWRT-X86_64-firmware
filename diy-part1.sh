41f7fb18891151cc582cfbb24fde72b7b8a01eb7
lovezanky <31689073+lovezanky@users.noreply.github.com>
2025-03-17 12:39:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,20 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# SmartDNS插件
+git clone https://github.com/pymumu/openwrt-smartdns package/openwrt-smartdns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+#强制关机插件
+git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+#自动关机插件
+git clone https://github.com/sirpdboy/luci-app-autopoweroff package/luci-app-autopoweroff
+
+# argon主题
+pushd package/lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argon
+popd
+# Edge主题
+git clone -b 18.06 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge
+
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
